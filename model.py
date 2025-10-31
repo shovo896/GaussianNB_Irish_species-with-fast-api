f4d5c03ebef8e57c7e99b2790a54ee5d048a7aa6
Shuvo <suvo10.info@gmail.com>
2025-10-31 13:10:39 +0600

    hjvjhcf

--- model.py
+++ model.py
@@ -2,4 +2,20 @@ from fastapi import FastAPI
 import uvicorn 
 from sklearn.datasets import load_iris 
 from sklearn.naive_bayes import GaussianNB 
-from pydantic import BaseModel 
+from pydantic import 
+app=FastAPI()
+
+class request_body(BaseModel) : 
+    sepal_length : float 
+    sepal_width  : float 
+    petal_length : float 
+    petal_width : float 
+
+iris=load_iris()
+#Getting our features and targets 
+
+x=iris.data 
+y=iris.target
+
+clf=GaussianNB()
+clf.fit(x,y)
